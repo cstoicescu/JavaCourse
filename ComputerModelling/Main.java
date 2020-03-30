9ad5cfb8988b2e7c2b6287adeb270f7b9eb629c1
Catalin Stoicescu <stoicescu.catalinn97@gmail.com>
2020-03-30 22:51:01 +0300

    modelling computer process and structure

---
+++ ComputerModelling/Main.java
@@ -0,0 +1,21 @@
+package ComputerModelling;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("2208","Dell","240V",dimensions);
+        Monitor theMonitor = new Monitor("27 inch","Acer",27,new Resolution(2540,1440));
+        Motherboard theMotherboard = new Motherboard("BJ-200","Asus",4,6,"v.2.44");
+        Computer pc = new Computer(theCase,theMonitor,theMotherboard);
+
+        pc.getTheCase().powerButton();
+        pc.powerUpAndDown();
+        pc.getTheMotherboard().loadProgram("Windows 10");
+        pc.openPaint();
+        pc.powerUpAndDown();
+
+
+        // Idea : Implement a menu , pc specs
+    }
+}
